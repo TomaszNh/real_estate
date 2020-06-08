c8e0eacaf7aa0e205b3658c7aba4a65bfa7c25a2
TomaszNh <tomasznohanowicz.zs@gmail.com>
2020-06-08 19:15:49 +0200

    08.06.20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,13 @@
-/*
-	Snapshot by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+
 
 (function($) {
 
 	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
 	});
 
 	$(function() {
@@ -39,10 +35,98 @@
 				);
 			});
 
-		// Scrolly.
-			$('.scrolly').scrolly();
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
 
-		// Gallery.
+		// Banner.
+			var $banner = $('#banner');
+
+			if ($banner.length > 0) {
+
+				// IE fix.
+					if (skel.vars.IEVersion < 12) {
+
+						$window.on('resize', function() {
+
+							var wh = $window.height() * 0.60,
+								bh = $banner.height();
+
+							$banner.css('height', 'auto');
+
+							window.setTimeout(function() {
+
+								if (bh < wh)
+									$banner.css('height', wh + 'px');
+
+							}, 0);
+
+						});
+
+						$window.on('load', function() {
+							$window.triggerHandler('resize');
+						});
+
+					}
+
+
+
+
+				// More button.
+					$banner.find('.more')
+						.addClass('scrolly');
+
+			}
+
+		// Tabbed Boxes
+
+			$('.flex-tabs').each( function() {
+
+				var t 		= jQuery(this),
+					tab 	= t.find('.tab-list li a'),
+					tabs 	= t.find('.tab');
+
+				tab.click(function(e) {
+
+					var x = jQuery(this),
+						y = x.data('tab');
+
+					// Set Classes on Tabs
+						tab.removeClass('active');
+						x.addClass('active');
+
+					// Show/Hide Tab Content
+						tabs.removeClass('active');
+						t.find('.' + y).addClass('active');
+
+					e.preventDefault();
+
+				});
+
+			});
+
+		// Scrolly.
+			if ( $( ".scrolly" ).length ) {
+
+				var $height = $('#header').height();
+
+				$('.scrolly').scrolly({
+					offset: $height
+				});
+			}
+
+	});
+
+// Gallery.
 			$('.gallery').each(function() {
 
 				var	$gallery = $(this),
@@ -52,53 +136,8 @@
 					$content.poptrox({
 						usePopupCaption: true
 					});
-
-				// Tabs.
-					$gallery.each( function() {
-
-						var $this = $(this),
-							$tabs = $this.find('.tabs a'),
-							$media = $this.find('.media');
-
-						$tabs.on('click', function(e) {
-
-							var $this = $(this),
-								tag = $this.data('tag');
-
-							// Prevent default.
-							 	e.preventDefault();
-
-							// Remove active class from all tabs.
-								$tabs.removeClass('active');
-
-							// Reapply active class to current tab.
-								$this.addClass('active');
-
-							// Hide media that do not have the same class as the clicked tab.
-								$media
-									.fadeOut('fast')
-									.each(function() {
-
-										var $this = $(this);
-
-										if ($this.hasClass(tag))
-											$this
-												.fadeOut('fast')
-												.delay(200)
-												.queue(function(next) {
-													$this.fadeIn();
-													next();
-												});
-
-									});
-
-						});
-
-					});
-
-
 			});
 
-	});
+// 
 
 })(jQuery);
